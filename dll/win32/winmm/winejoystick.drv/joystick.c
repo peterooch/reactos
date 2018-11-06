dc0ce0859fda4e7b62b5e4252340f833ee194abd
Baruch Rutman <peterooch@gmail.com>
2018-11-06 23:08:38 +0200

    [WINEJOYSTICK] Add stub

---
+++ dll/win32/winmm/winejoystick.drv/joystick.c
@@ -0,0 +1,66 @@
+/*
+ * WinMM joystick driver common code
+ *
+ * Copyright 1997 Andreas Mohr
+ * Copyright 2000 Wolfgang Schwotzer
+ * Copyright 2002 David Hagood
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include "joystick.h"
+
+
+/**************************************************************************
+ *                              DriverProc (JOYSTICK.@)
+ */
+LRESULT CALLBACK JSTCK_DriverProc(DWORD_PTR dwDevID, HDRVR hDriv, UINT wMsg, LPARAM dwParam1, LPARAM dwParam2)
+{
+    switch(wMsg) {
+    case DRV_LOAD:              return 1;
+    case DRV_FREE:              return 1;
+#ifdef __REACTOS__
+    case DRV_OPEN:              return 0;
+    case DRV_CLOSE:             return 0;
+#else
+    case DRV_OPEN:              return driver_open((LPSTR)dwParam1, dwParam2);
+    case DRV_CLOSE:             return driver_close(dwDevID);
+#endif
+    case DRV_ENABLE:            return 1;
+    case DRV_DISABLE:           return 1;
+    case DRV_QUERYCONFIGURE:    return 1;
+    case DRV_CONFIGURE:         MessageBoxA(0, "JoyStick MultiMedia Driver !", "JoyStick Driver", MB_OK);       return 1;
+    case DRV_INSTALL:           return DRVCNF_RESTART;
+    case DRV_REMOVE:            return DRVCNF_RESTART;
+
+    case JDD_GETNUMDEVS:        return 1;
+#ifdef __REACTOS__
+    case JDD_GETDEVCAPS:        return 0;
+    case JDD_GETPOS:            return 0;
+#else
+    case JDD_GETDEVCAPS:        return driver_joyGetDevCaps(dwDevID, (LPJOYCAPSW)dwParam1, dwParam2);
+    case JDD_GETPOS:            return driver_joyGetPos(dwDevID, (LPJOYINFO)dwParam1);
+#endif
+    case JDD_SETCALIBRATION:
+    case JDD_CONFIGCHANGED:     return JOYERR_NOCANDO;
+#ifdef __REACTOS__
+    case JDD_GETPOSEX:          return 0;
+#else
+    case JDD_GETPOSEX:          return driver_joyGetPosEx(dwDevID, (LPJOYINFOEX)dwParam1);
+#endif
+    default:
+        return DefDriverProc(dwDevID, hDriv, wMsg, dwParam1, dwParam2);
+    }
+}
