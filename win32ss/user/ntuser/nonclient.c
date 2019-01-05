62e0420ef15e9c8e3d2d85caebd0c457efa6c2d0
Baruch Rutman <peterooch@gmail.com>
2019-01-05 19:48:58 +0200

    [WIN32SS] Attempt to fix RTL caption bars
    Text and icon are drawn in the correct places, sysmenu buttons refuse to be drawn

--- win32ss/user/ntuser/nonclient.c
+++ win32ss/user/ntuser/nonclient.c
@@ -37,6 +37,13 @@ DBG_DEFAULT_CHANNEL(UserDefwnd);
              (WindowRect.right - WindowRect.left == ParentClientRect.right) && \
              (WindowRect.bottom - WindowRect.top == ParentClientRect.bottom)))
 
+FORCEINLINE VOID mirror_rect(const RECT *window_rect, LPRECT rect)
+{
+    int width = window_rect->right - window_rect->left;
+    int tmp = rect->left;
+    rect->left = width - rect->right;              
+    rect->right = width - tmp;
+}
 
 VOID FASTCALL
 UserDrawWindowFrame(HDC hdc,
@@ -709,6 +716,9 @@ UserDrawCaptionButton(PWND pWnd, LPRECT Rect, DWORD Style, DWORD ExStyle, HDC hD
          TempRect.top += 2;
          TempRect.right -= 1;
 
+         if (ExStyle & WS_EX_LAYOUTRTL)
+            mirror_rect(Rect, &TempRect);
+
          DrawFrameControl(hDC, &TempRect, DFC_CAPTION,
                           ((Style & WS_MINIMIZE) ? DFCS_CAPTIONRESTORE : DFCS_CAPTIONMIN) |
                           (bDown ? DFCS_PUSHED : 0) |
@@ -727,6 +737,9 @@ UserDrawCaptionButton(PWND pWnd, LPRECT Rect, DWORD Style, DWORD ExStyle, HDC hD
          TempRect.bottom = TempRect.top + UserGetSystemMetrics(SM_CYSIZE) - 2;
          TempRect.top += 2;
          TempRect.right -= 1;
+
+         if (ExStyle & WS_EX_LAYOUTRTL)
+             mirror_rect(Rect, &TempRect);
 
          DrawFrameControl(hDC, &TempRect, DFC_CAPTION,
                           ((Style & WS_MAXIMIZE) ? DFCS_CAPTIONRESTORE : DFCS_CAPTIONMAX) |
@@ -752,6 +765,21 @@ UserDrawCaptionButton(PWND pWnd, LPRECT Rect, DWORD Style, DWORD ExStyle, HDC hD
          }
          TempRect.top += 2;
          TempRect.right -= 2;
+#if 0
+         if (ExStyle & WS_EX_LAYOUTRTL)
+         {
+             RECT MirroredRect;
+
+             MirroredRect.top = TempRect.top;
+             MirroredRect.bottom = TempRect.bottom;
+             MirroredRect.right = (Rect->right - TempRect.left);
+             MirroredRect.left = MirroredRect.right - (TempRect.right - TempRect.right);
+             TempRect = MirroredRect;
+         }
+#endif
+         if (ExStyle & WS_EX_LAYOUTRTL)
+             mirror_rect(Rect, &TempRect);
+
 
          DrawFrameControl(hDC, &TempRect, DFC_CAPTION,
                           (DFCS_CAPTIONCLOSE | (bDown ? DFCS_PUSHED : 0) |
