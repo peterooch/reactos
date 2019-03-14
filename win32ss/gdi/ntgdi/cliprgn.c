d5551ead056cc52553cf5dd5105d1a8762cac06e
Baruch Rutman <peterooch@gmail.com>
2019-03-14 03:48:29 +0200

    [NTGDI] Mirror rect in NtGdiIntersectClipRect
    Makes many things to draw correctly.

--- win32ss/gdi/ntgdi/cliprgn.c
+++ win32ss/gdi/ntgdi/cliprgn.c
@@ -318,6 +318,9 @@ NtGdiIntersectClipRect(
     rect.bottom = yBottom;
     IntLPtoDP(pdc, (LPPOINT)&rect, 2);
 
+    if (pdc->pdcattr->dwLayout & LAYOUT_RTL)
+        IntMirrorCoords(pdc, &rect, MIRROR_RECT);
+
     /* Check if we already have a clip region */
     if (pdc->dclevel.prgnClip != NULL)
     {
